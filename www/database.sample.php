09ecd0dc131e58bc8eee6c7e6dd91c5518a34cda
HanbitGaram <webmaster@hanb.jp>
2018-09-24 13:48:50 +0900

    데이터베이스 샘플 파일 제작

---
+++ www/database.sample.php
@@ -0,0 +1,10 @@
+<?php
+	// 본 파일 사용시, 파일명을 database.php 로 변경하여 사용하십시오.
+	if(!defined('_ANIDB_')) exit; // 개별 파일 접근 제한.
+	
+	define('DB_HOST', 'localhost');
+	define('DB_ID', 'root');
+	define('DB_PW', 'hanbitgaram_server_1');
+	define('DB_PORT', '3306');
+	define('DB_NAME', 'anime');
+	define('DB_PREFIX', 'tgi_');
