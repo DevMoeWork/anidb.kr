8e78fa26a1af1da3e0738831ad5418bcbd8d7792
HanbitGaram <webmaster@hanb.jp>
2018-09-25 20:15:15 +0900

    test2

--- www/lib/general.lib.php
+++ www/lib/general.lib.php
@@ -3,12 +3,13 @@
 
 	// 파일 끝에 반드시 추가해야할 코드
 	function stream_end(){
-		function head_stylesheet($link,$order){
-			echo $link;
-		}
-		function head_javascript($script, $order){
-		
-		}
 		$contents = ob_get_contents();
 		ob_end_flush();
+	}
+
+	function head_stylesheet($link,$order){
+		echo $link;
+	}
+	function head_javascript($script, $order){
+
 	}
