b147747f1bc99073afc7c03980e21a4f49e12a5b
HanbitGaram <webmaster@hanb.jp>
2018-09-26 09:27:33 +0900

    오타수정

--- www/index.php
+++ www/index.php
@@ -2,6 +2,6 @@
 	include_once('./sysconfig.php');
 	include_once(ANI_PATH.'/head.php');
 	
-	include_once ANI_LAYOUT_MAIN_PATH.'/layout.php';
+	include_once ANI_LAYOUT_MAIN_PATH.'/index.php';
 
 	include_once(ANI_PATH.'/tail.php');
--- www/layout/default/head.php
+++ www/layout/default/head.php
@@ -3,4 +3,4 @@
 
 	include_once(ANI_LAYOUT_PUBLIC_PATH.'/head.sub.php');
 	head_metatag('<meta name="viewport" content="width=device-width, initial-scale=1">');
-	head_stylesheet('<link rel="stylesheet" href="'.ANI_CSS_PATH.'/w3.css">');
+	head_stylesheet('<link rel="stylesheet" href="'.ANI_CSS_URL.'/w3.css">');
