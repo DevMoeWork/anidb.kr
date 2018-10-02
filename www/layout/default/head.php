2e748d07dfac92b7c87e542ef1e22340fc1bd493
HanbitGaram <webmaster@hanb.jp>
2018-10-02 22:24:46 +0900

    코드 오타 수정

--- www/layout/default/head.php
+++ www/layout/default/head.php
@@ -14,7 +14,7 @@
     <a href="#" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-text-teal"><i class="fa fa-th-large fa-fw w3-margin-right"></i>Anime DB</a>
 	 <?php for($i=0; $i<count($genre); $i++): ?>
 	 <a href="<?php echo ANI_URL; ?>/g/<?php echo rawurlencode($genre[$i]); ?>" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-bookmark fa-fw w3-margin-right"></i><?php echo htmlspecialchars($genre[$i]); ?></a>
-	 <?php endif; ?>
+	 <?php endfor; ?>
     <a href="https://moe.work" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-user fa-fw w3-margin-right"></i>MOEWORK</a>
     <a href="mailto:keffred@moe.work?subject=CONTACT" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-envelope fa-fw w3-margin-right"></i>CONTACT</a>
   </div>
